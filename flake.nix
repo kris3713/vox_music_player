c669bb22c58a654bc299350cdb066cedbd8a4de6
kris3713 <07e5297d5b@c0x0.com>
2026-01-30 15:56:51 -0700

    Refactored flake.nix
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- flake.nix
+++ flake.nix
@@ -1,64 +1,61 @@
 {
-  description = "An empty flake template that you can adapt to your own environment";
+  description = "kris3713's nix flake for flutter development";
 
   # Flake inputs
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*"; # Stable Nixpkgs (use 0.1 for unstable)
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*";
 
   # Flake outputs
-  outputs =
-    { self, ... }@inputs:
-    let
-      # The systems supported for this flake's outputs
-      supportedSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-      ];
+  outputs = {self, ...} @ inputs: let
+    # The systems supported for this flake's outputs
+    supportedSystems = [
+      "x86_64-linux" # 64-bit Intel/AMD Linux
+      "aarch64-linux" # 64-bit ARM Linux
+    ];
 
-      # Helper for providing system-specific attributes
-      forEachSupportedSystem =
-        f:
-        inputs.nixpkgs.lib.genAttrs supportedSystems (
+    # Helper for providing system-specific attributes
+    forEachSupportedSystem = with inputs;
+      f:
+        nixpkgs.lib.genAttrs supportedSystems (
           system:
-          f {
-            inherit system;
-            # Provides a system-specific, configured Nixpkgs
-            pkgs = import inputs.nixpkgs {
+            f {
               inherit system;
-              # Enable using unfree packages
-              config.allowUnfree = true;
-            };
-          }
+              # Provides a system-specific, configured Nixpkgs
+              pkgs = import nixpkgs {
+                inherit system;
+                # Enable using unfree packages
+                config.allowUnfree = true;
+              };
+            }
         );
-    in
-    {
-      # Development environments output by this flake
+  in {
+    # Development environments output by this flake
 
-      # To activate the default environment:
-      # nix develop
-      # Or if you use direnv:
-      # direnv allow
-      devShells = forEachSupportedSystem (
-        { pkgs, system }:
-        {
-          # Run `nix develop` to activate this environment or `direnv allow` if you have direnv installed
-          default = pkgs.mkShellNoCC {
-            # The Nix packages provided in the environment
-            packages = with pkgs; [
-              # flutter pkg is provided by mise
-              cmake
-              clang
-              ninja
-              glib.dev
-              gtk3.dev
-            ];
+    # To activate the default environment:
+    # nix develop
+    # Or if you use direnv:
+    # direnv allow
+    devShells = forEachSupportedSystem ({
+      pkgs,
+      system,
+    }: {
+      # Run `nix develop` to activate this environment or `direnv allow` if you have direnv installed
+      default = pkgs.mkShellNoCC {
+        # The Nix packages provided in the environment
+        packages = with pkgs; [
+          # flutter pkg is provided by mise
+          cmake
+          clang
+          ninja
+          glib.dev
+          gtk3.dev
+        ];
 
-            # Set any environment variables for your development environment
-            env = { };
+        # Set any environment variables for your development environment
+        env = {};
 
-            # Add any shell logic you want executed when the environment is activated
-            shellHook = "";
-          };
-        }
-      );
-    };
+        # Add any shell logic you want executed when the environment is activated
+        shellHook = "";
+      };
+    });
+  };
 }
